a15c4ca6263f432d48fbec826415a0d848743e62
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-11-14 19:46:33 -0500

    started some framework for game

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,6 +1,9 @@
 package com.github.zipcodewilmington;
 
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * @author xt0fer
  * @version 1.0.0
@@ -9,6 +12,29 @@ package com.github.zipcodewilmington;
 
 //nina
 public class Hangman {
+
+    public Hangman() {}
+
+    public static void main(String[] args) {
+        Hangman game = new Hangman();
+        game.runGame();
+    }
+
+    Scanner scanner = new Scanner(System.in);
+    Random rand = new Random();
+    int wordGenerator = rand.nextInt(3);
+    int numOfGuesses = 0;
+    String[] wordBank = {"bye", "hog", "and"};
+
+    public void runGame() {
+
+    }
+
+    public printArray(char[] charArray) {
+        for (int i = 0; i < charArray.length; i++) {
+
+        }
+    }
 
 
 
--- src/test/java/com/github/zipcodewilmington/HangmanTest.java
+++ src/test/java/com/github/zipcodewilmington/HangmanTest.java
@@ -1,9 +1,20 @@
 package com.github.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * @author xt0fer
  * @version 1.0.0
  * @date 5/27/21 11:02 AM
  */
 public class HangmanTest {
+
+//    @Test
+//    public void testHangman() {
+//        Assert.assertEquals();
+//    }
+
+
+
 }
